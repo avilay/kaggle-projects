c34fc59b5f309775e3640cc0b09488fe53dd2f89
Avilay <avilay@sagemker>
2021-08-18 23:07:56 +0000

    fixing imports

--- higgs-pred/higgs/__init__.py
+++ higgs-pred/higgs/__init__.py
@@ -1,4 +1,15 @@
 # flake8: noqa
-from .consts import COLNAMES, DATAROOT
+from .consts import (
+    COLNAMES, 
+    DATAROOT, 
+    RUNROOT, 
+    PROJECT, 
+    HIGGS_CSV,
+    HIGGS_TRAIN_CSV,
+    HIGGS_TEST_CSV,
+    SAMPLE_HIGGS_CSV
+)    
+
 from .higgs_data_module import HiggsDataModule
 from .higgs_classifier import HiggsClassifier
+from .main import sample, split, train
